050d522a08337f9825388b74495db8504e5c0264
Johannes Wünsche <johannes@spacesnek.rocks>
2022-01-23 21:25:10 +0100

    Add comment about SendWrapper

--- src/messages.rs
+++ src/messages.rs
@@ -5,6 +5,8 @@ use std::rc::Rc;
 
 #[derive(Debug)]
 pub enum WorkerMessage {
+    // This is currently a hack, we never construct this message from another thread than the main one. But other
+    // varians might just be, so the SendWrapper is here to allow us to keep this as one message and keep the main events as one channel receiver.
     AddOutput(SendWrapper<Rc<RwLock<Output>>>, u32),
     RemoveOutput(u32),
     AnimationStep(f32),
